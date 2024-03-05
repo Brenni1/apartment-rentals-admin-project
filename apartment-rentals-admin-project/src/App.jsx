0c1f03ea4b2eb1ff69e04bcb5b37dddf64ef8d55
Brenni1 <phillip.brenndoerfer@gmail.com>
2024-03-05 17:59:18 +0100

    Did most of Day2

--- apartment-rentals-admin-project/src/App.jsx
+++ apartment-rentals-admin-project/src/App.jsx
@@ -3,12 +3,14 @@ import "./components/Navbar.jsx";
 import NavBar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
 import Sidebar from "./components/Sidebar.jsx";
+import List from "./components/List.jsx";
 function App() {
   return (
     <div>
       <NavBar></NavBar>
       <Footer></Footer>
       <Sidebar></Sidebar>
+      <List />
     </div>
   );
 }
--- apartment-rentals-admin-project/src/components/Navbar.jsx
+++ apartment-rentals-admin-project/src/components/Navbar.jsx
@@ -3,8 +3,10 @@ import logoImage from "../assets/apartment-logo.png";
 const NavBar = () => {
   return (
     <div id="navBar">
-      <h1>Apartment Rental Company</h1>
-      <img src={logoImage} alt="Company Logo" />
+      <h1>
+        <img src={logoImage} alt="Company Logo" />
+        Apartment Rental Company
+      </h1>
     </div>
   );
 };
